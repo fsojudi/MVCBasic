66c7036c8d4f85e403018ac3d572913d2f74e561
fsojudi@gmail.com <fsojudi@gmail.com>
2022-04-19 00:26:30 +0200

    Part 1

--- MVCBasic/Startup.cs
+++ MVCBasic/Startup.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MVCBasic
+namespace MVCBasicsAssignment
 {
     public class Startup
     {
@@ -16,6 +16,7 @@ namespace MVCBasic
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +27,17 @@ namespace MVCBasic
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                   name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                    );
             });
+
         }
     }
 }
