50740ff81b9b6556eb6a8e2a13adbf7395d952e5
fsojudi@gmail.com <fsojudi@gmail.com>
2022-04-20 00:42:50 +0200

    part 3

---
+++ MVCBasic/Controllers/GuessingGame.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCBasic.Controllers
+{
+    public class GuessingGame : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
