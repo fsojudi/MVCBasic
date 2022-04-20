1d7a1c379519ffacf000b7141479326837a3462e
fsojudi@gmail.com <fsojudi@gmail.com>
2022-04-20 18:10:13 +0200

    updateing part 3

--- MVCBasic/Controllers/GuessingGame.cs
+++ MVCBasic/Controllers/GuessingGame.cs
@@ -3,13 +3,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MVCBasic.Models;
 
 namespace MVCBasic.Controllers
 {
     public class GuessingGame : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(int guessNumber)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(100);
+            ViewBag.Result1 = randomNumber;
+            ViewBag.Result2 = Game.CheckGame(guessNumber,randomNumber);
+       
             return View();
         }
     }
---
+++ MVCBasic/Models/Game.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCBasic.Models
+{
+    public class Game
+    {
+        public static string CheckGame(int guessNumber, int randomNumber )
+        {
+            string result= "";
+            
+            if (randomNumber == guessNumber)
+            {
+                result = "Hurray! Your guessed number is correct.";
+            }
+
+            else if (guessNumber < randomNumber)
+            {
+                result = "Sorry!Your guessed number is too low.";
+
+            }
+
+            else if (guessNumber > randomNumber)
+            {
+                result = "Your guessed number is too high.";
+
+            }
+
+            return result;
+
+        }
+    }
+}
