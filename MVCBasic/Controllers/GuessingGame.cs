6786ab2bf7b1996b768667e208f8187d4b4bd477
fsojudi@gmail.com <fsojudi@gmail.com>
2022-04-21 12:38:44 +0200

    some changes

--- MVCBasic/Controllers/GuessingGame.cs
+++ MVCBasic/Controllers/GuessingGame.cs
@@ -19,9 +19,13 @@ namespace MVCBasic.Controllers
         {
             Random random = new Random();
             int randomNumber = random.Next(100);
+            int guess = 1;
+            while ( guess<10)
+            {
             ViewBag.Result1 = randomNumber;
             ViewBag.Result2 = Game.CheckGame(guessNumber,randomNumber);
-       
+                guess++;
+            }
             return View();
         }
     }
